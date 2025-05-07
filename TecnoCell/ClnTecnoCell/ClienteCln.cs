71d93548fd326ec1fec5f408f2ffea424dd91bb1
Rodriguez_Darwin <darwin.erh@gmail.com>
2025-05-06 22:38:39 -0400

    Cln Entidades

---
+++ TecnoCell/ClnTecnoCell/ClienteCln.cs
@@ -0,0 +1,14 @@
+﻿using CadTecnoCell;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClnTecnoCell
+{
+    public class ClienteCln
+    {
+    }
+}
